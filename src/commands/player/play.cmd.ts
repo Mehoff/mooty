f7a23ab29946dcac7ed474d350ac8acaa5aeff6e
Mehoff <mixov2000@gmail.com>
2022-08-30 22:15:29 +0300

    YTDLService initial

--- src/commands/player/play.cmd.ts
+++ src/commands/player/play.cmd.ts
@@ -4,6 +4,7 @@ import {
   SlashCommandBuilder,
 } from "discord.js";
 import { Command } from "../../../types";
+import { YTDLService } from "../../services/player.service";
 
 const Play: Command = {
   data: new SlashCommandBuilder()
@@ -14,10 +15,15 @@ const Play: Command = {
         .setName("query")
         .setDescription("Enter video URL or search input")
         .setRequired(true)
+        .setMinLength(3)
+        .setMaxLength(499)
     ),
   execute: async (interaction: ChatInputCommandInteraction<CacheType>) => {
     const query = interaction.options.getString("query");
-    return await interaction.reply(query ? query : "Null query");
+    if (!query) return await interaction.reply("Bad query, try again");
+
+    const result = await YTDLService.handleQuery(query);
+    if (result) return await interaction.reply(result);
   },
 };
 
--- src/services/player.service.ts
+++ src/services/player.service.ts
@@ -1,1 +1,15 @@
-export class PlayerService {}
+import ytdl from "ytdl-core";
+
+export class YTDLService {
+  public static async handleQuery(query: string) {
+    return ytdl.validateURL(query)
+      ? await this.playSongByURL(query)
+      : await this.playSongByQuery(query);
+  }
+
+  public static async playSongByURL(url: string) {
+    const basicInfo = await ytdl.getBasicInfo(url);
+    return basicInfo.videoDetails.title;
+  }
+  public static async playSongByQuery(query: string) {}
+}
